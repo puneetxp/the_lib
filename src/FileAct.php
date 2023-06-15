3a103648bb7c9d7f3033009f426df4e4cb758967
Puneet Sharma <puneetsharma9@hotmail.com>
2023-06-15 08:47:43 +0530

    add some file action

--- src/FileAct.php
+++ src/FileAct.php
@@ -105,4 +105,18 @@ class FileAct
    {
       unlink($path);
    }
+
+   public static function fopen_dir($link)
+   {
+      $filename = $link;
+      $dirname = dirname($filename);
+      if (!is_dir($dirname)) {
+         mkdir($dirname, 0755, true);
+      }
+      return fopen($filename, 'w');
+   }
+   public static function createfile($dir, $string)
+   {
+      fwrite(Self::fopen_dir($dir), $string);
+   }
 }
