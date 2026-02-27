5424f6a484615f0b16eece05ff3ec1a00f7a43aa
puneetxpdream <puneet@dreamcast.co>
2026-02-27 10:53:53 +0530

    Add enable filter to page block query in PageBase constructor

--- src/PageBase.php
+++ src/PageBase.php
@@ -11,7 +11,7 @@ abstract class PageBase
     {
         if (class_exists('\App\Model\Page')) {
             if (self::$pageCache === null) {
-                $blocks = \App\Model\Page::where(['slug REGEXP' => "^"])->get()->with(['page_block'])->array()["page_block"];
+                $blocks = \App\Model\Page::where(['slug REGEXP' => "^","enable" => 1])->get()->with(['page_block'])->array()["page_block"];
                 self::$pageCache = array_merge(['page_block' => $blocks], ...array_map(fn($x) => [$x["component"] => $x], $blocks));
             }
             $this->page = self::$pageCache;
